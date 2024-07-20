874dd92de5d63ae327d00a90dc23671a3da6f213
Yuzo <youzow3@yahoo.co.jp>
2024-07-20 15:13:50 +0900

    Make version 3 searching and generating code

--- search.py
+++ search.py
@@ -21,7 +21,7 @@ class Application:
         self.messages: list[dict[str, str]] = []
 
     def analyze_is_useful_v2(self, title: str, description: str) -> bool:
-        b: bool = self.generate_bool(f"Does this website seem to be useful?\nTitle:{title}\nDescription:{description}", max_attempt = 1)
+        b: bool = self.generate_bool(f"Does this website seem to be useful?\nTitle:{title}\nDescription:{description}", save = False, max_attempt = 1)
         return False if b is None else b
 
     def analyze_load_website(self, url: str) -> str:
@@ -60,23 +60,44 @@ class Application:
 
         return points
 
-    def execute_v2(self, plan: ET.Element) -> str | None:
-        search_result: list[googlesearch.SearchResult] = []
-        analyze_result: list[str] = []
+    def analyze_v3_1(self, result: googlesearch.SearchResult, messages: list[dict[str, str]] = [])-> list[str]:
+        markdown: str = self.analyze_load_website(result.url)
+        instruction: str = "Pickup useful information to provide accurate information to the user."
+        messages.append({ "role": "system", "content": markdown })
+        return self.generate_list(instruction, messages = messages, save = False, max_attempt = 3)
 
-        for t in plan.findall("action"):
-            name: str = t.get("name")
-            goal: str = t.get("goal")
+    def execute_v3(self, plan: list[str]) -> tuple[str, list[googlesearch.SearchResult]]:
+        result: dict[googlesearch.SearchResult, list[str]] = None
 
-            if name == "search":
-                search_result = self.search_v2()
-            elif name == "analyze":
-                analyze_result = self.analyze_v3(search_result)
-            elif name == "summarize":
-                return self.summarize_v3(analyze_result)
-        return None
+        while True:
+            ic(plan)
+            for p in plan:
+                cmd = p.split()
+                if cmd[0] == "plan":
+                    plan = self.plan_dynamic(result)
+                    break
+                if cmd[0] == "search":
+                    result_table: dict[str, int] = {"fast": 3, "medium": 5, "slow": 7}
+                    num_results = result_table["medium"]
 
-    def generate(self, messages: list[dict[str, str]] = [], verify: Callable[[str], bool] = lambda x: True, max_attempt: int = -1, save = False, prefix: str = "", suffix: str = "", **kwargs) -> str | None:
+                    if len(cmd) > 1:
+                        num_results = result_table[cmd[1]]
+
+                    result = self.search_v3(num_results)
+                elif cmd[0] == "summarize":
+                    return self.summarize_v3(result), list(result.keys())
+
+    def generate(
+        self,
+        messages: list[dict[str, str]] = [],
+        verify: Callable[[str], bool] = lambda x: True,
+        max_attempt: int = -1,
+        save = True,
+        save_func: Callable[[list[dict[str, str]], str], list[dict[str, str]]] = lambda x, y: x + [{ "role": "assistant", "content": y }],
+        prefix: str = "",
+        suffix: str = "",
+        **kwargs
+    ) -> str | None:
         attempt: int = 0
 
         while True:
@@ -89,29 +110,46 @@ class Application:
             ic(content)
 
             if verify(content):
+                ic(messages)
                 if save:
-                    self.messages = messages + [{ "assistant", content }]
+                    self.messages += save_func(messages, content)
                 return content
     
-    def generate_xml(self, examples: str | list[str], root_tag: str, children: dict[str, Any], instruction: str = None, **kwargs) -> ET.Element | None:
+    def generate_xml(
+            self,
+            examples: str | list[str],
+            root_tag: str,
+            children: dict[str, Any],
+            instruction: str = None,
+            verify: Callable[[ET.Element], bool] = lambda x: True,
+            messages: list[dict[str, str]] = [],
+            save_func: Callable[[ET.Element], str] = lambda y: y,
+            **kwargs
+        ) -> ET.Element | None:
+        def __save_func(x: list[dict[str, str]], y: str) -> list[dict[str, str]]:
+            return messages + ([] if instruction is None else [{ "role": "system", "content": instruction}]) + [{ "role": "assistant", "content": save_func(ET.fromstring(y))}]
+
         attempt: int = 0
 
         __instruction: str
         __verify: Callable[[str], bool]
-        __instruction, __verify = self.xml_instruction_and_verify(examples, root_tag, children)
+        __instruction, __verify = self.xml_instruction_and_verify(examples, root_tag, children, verify = verify)
 
-        messages: list[dict[str, str]] = []
+        __messages: list[dict[str, str]] = []
         if instruction is not None:
-            messages.append({ "role": "system", "content": instruction })
-        messages.append({ "role": "system", "content": __instruction })
+            __messages.append({ "role": "system", "content": instruction })
+        __messages.append({ "role": "system", "content": __instruction })
 
-        content: str = self.generate(messages, __verify, suffix = f"</{root_tag}>", stop = f"</{root_tag}>", **kwargs)
+        content: str = self.generate(messages + __messages, __verify, suffix = f"</{root_tag}>", stop = f"</{root_tag}>", save_func = __save_func, **kwargs)
         ic("generate_xml", content)
-        element: ET.Element = ET.fromstring(content)
-
-        return element
+        if content is None:
+            return None
+        return ET.fromstring(content)
 
     def generate_list(self, instruction: str, **kwargs) -> list[str] | None:
+        def __save_func(y: ET.Element):
+            return '\n'.join([f"* {yi.text}" for yi in y.findall("item")]).lstrip()
+
         example: str = """
         <list>
             <item>Item 1</item>
@@ -124,31 +162,40 @@ class Application:
             "<item>": "item in the list"
         }
 
-        __instruction: str = instruction + "\nYou have to make List in XML."
-
-        element: ET.Element = self.generate_xml(example, "list", children, __instruction, **kwargs)
+        element: ET.Element = self.generate_xml(example, "list", children, instruction, save_func = __save_func, **kwargs)
+        if element is None:
+            return None
         
         return [e.text for e in element.findall("item")]
 
-    def generate_bool(self, instruction: str, **kwargs) -> bool | None:
-        examples: list[str] = [ "<value>True</value>", "<value>False</value>" ]
+    def generate_bool(self, instruction: str, max_attempt = -1, **kwargs) -> bool | None:
+        def __verify(xml: ET.Element) -> bool:
+            return xml.text.lower() in ["true", "false"]
+
+        def __save_func(y: ET.Element) -> str:
+            return y.text.lower()
+
+        examples: list[str] = [ "<bool>True</bool>", "<bool>False</bool>" ]
         children: dict[str, str] = {}
-        __instruction: str = instruction + "\nYou have to make Bool value in XML"
         
-        element: ET.Element = self.generate_xml(examples, "value", children, __instruction, **kwargs)
+        element: ET.Element = self.generate_xml(examples, "bool", children, instruction, verify = __verify, save_func = __save_func, max_attempt = 1, **kwargs)
+        if element is None:
+            return None
+
         text: str = element.text.lower()
         if text == "true":
             return True
         elif text == "false":
             return False
-        return None
 
     def generate_string(self, instruction: str, **kwargs) -> str | None:
-        example: str = "<value>String</value>"
+        example: str = "<string>String Value</string>"
         children: dict[str, str] = {}
-        __instruction: str = instruction + "\nYou have to make String value in XML"
 
-        element: ET.Element = self.generate_xml(example, "value", children, __instruction, **kwargs)
+        element: ET.Element = self.generate_xml(example, "string", children, instruction, save_func = lambda y: y.text, **kwargs)
+        if element is None:
+            return None
+
         text: str = element.text
         return text
 
@@ -181,15 +228,59 @@ class Application:
         self.messages.append({ "role": "assistant", "content": xml_str })
         return ET.fromstring(xml_str)
 
+    def plan_v3(self, prompt: str) -> list[str]:
+        messages: list[dict[str, str]] = [
+            { "role": "user", "content": prompt },
+            { "role": "system", "content": "Read the user input. Then answer the questions." }
+        ]
+
+        if self.generate_bool("Do you have unclear term or words in the user prompt?", messages = messages):
+            return ["search fast", "plan"]
+
+        if self.generate_bool("Do you need user interaction first to provide more accurate information?"):
+            return ["plan"]
+
+        if self.generate_bool("Can you provide accurate information about this?"):
+            return ["summarize"]
+
+        if self.generate_bool("Do you need user interaction after/during search online?"):
+            return ["search", "plan"]
+
+        return ["search", "summarize"]
+
+    def plan_dynamic(self, past_result: dict[googlesearch.SearchResult, list[str]]) -> list[str]:
+        temp_summary: str
+
+        if past_result is None:
+            temp_summary = "You have to clarify things the user wants to know first."
+        else:
+            past_result_str: str = ""
+            for v in past_result.values():
+                for vi in v:
+                    past_result_str += f"{vi}\n"
+            temp_summary: str = self.generate_string(f"{past_result_str}Read the keypoints above and write summary.", save = False)
+
+        messages = [{ "role": "system", "content": temp_summary }]
+
+        instruction: str = "Make a question for the user to provide more accurate or specific answer."
+
+        question: str = self.generate_string(instruction)
+        answer: str = input(f"{question}:")
+
+        return self.plan_v3(answer)
+
     def run_v2(self) -> int:
         while True:
             prompt: int = input("Search > ")
             if prompt == "exit":
                 break
-            u: str = self.understand_v2(prompt)
-            plan: ET.Element = self.plan_v2()
-            result: str = self.execute_v2(plan)
+            plan: list[str] = self.plan_v3(prompt)
+            result: str
+            websites: list[googlesearch.SearchResult]
+            result, websites = self.execute_v3(plan)
+            print(websites)
             print(result)
+            ic(self.messages)
 
     def search(self, goal: str) -> list[googlesearch.SearchResult]:
         example: str = """
@@ -220,7 +311,7 @@ class Application:
 
         search_result: list[googlesearch.SearchResult] = []
         for keyword in keyword_list:
-            result: Generator[googlesearch.SearchResult] = googlesearch.search(keyword, num_results = 5, advanced = True, sleep_interval = 1, timeout = 60)
+            result: Generator[googlesearch.SearchResult] = googlesearch.search(keyword= 5, advanced = True, sleep_interval = 1, timeout = 60)
             _ = [search_result.append(r) for r in result]
 
         return search_result
@@ -236,6 +327,36 @@ class Application:
             _ = [search_result.append(r) for r in result]
 
         return search_result
+
+    def search_v3(self, num_results: int) -> dict[googlesearch.SearchResult, list[str]]:
+        keypoints: dict[searchresult.SearchResult, list[str]] = {}
+        keypoints_str: str = ""
+        keywords: list[str] = self.generate_list("Make search keywords to gather information online.")
+        info: list[str] = self.generate_list("List stuff that you should collect from the internet.")
+
+        for keyword in keywords:
+            for result in googlesearch.search(keyword, num_results = num_results, advanced = True, timeout = 60):
+                instruction: str = f"Title:{result.title}\nDescription:{result.title}\nDo you think this website contains necessary information?"
+                if not self.generate_bool(instruction, save = False):
+                    continue
+
+                messages = [{ "role": "system", "content": keypoints_str }]
+
+                result_keypoints: list[str] = self.analyze_v3_1(result, messages = messages)
+                if result_keypoints is None:
+                    continue
+                keypoints[result] = result_keypoints
+                
+                keypoints_str += '\n'.join(result_keypoints).lstrip()
+                messages[0]["content"] = keypoints_str
+
+                if self.generate_bool("Is the information enough to explain the user's question?", save = False):
+                    return keypoints
+
+                if self.generate_bool(f"Is the information about keyword:{keyword} enough?", save = False):
+                    break
+
+        return keypoints
 
     def summarize_v2(self, goal: str, analyze_result: dict[googlesearch.SearchResult, tuple[list[str], list[str]]]) -> str:
         example: str = """
@@ -297,12 +418,20 @@ class Application:
         return ET.tostring(xml)
 
     def summarize_v3(self, analyze_result: dict[googlesearch.SearchResult, list[str]]) -> str:
-        instruction: str = ""
-        for k, p in analyze_result.items():
-            for pi in p:
-                instruction += f"{pi}\n"
-        instruction += "Read the input above and summarize it."
-        return self.generate_string(instruction)
+        messages: list[dict[str, str]] = []
+        instruction: str = "Write the explanation from gathered information"
+
+        if analyze_result is None:
+            instruction = "Write the explanation without uncertain things."
+        else:
+            analyze_result_str: str = ""
+            for k, p in analyze_result.items():
+                for pi in p:
+                    analyze_result_str += f"{pi}\n"
+            messages.append({ "role": "system", "content": analyze_result_str })
+            instruction = "Write the explanation from gathered information."
+
+        return self.generate_string(instruction, messages = messages)
 
     def understand_v2(self, prompt: str) -> None:
         example: str = """
@@ -332,6 +461,7 @@ class Application:
         self.messages += [
             { "role": "assistant", "content": xml_str }
         ]
+
 
     def verify_xml_v2(self, root_tag: str, children: dict[str, Any], verify: Callable[[ET.Element], bool] = lambda x: True) -> Callable[[str], bool]:
         def __verify(xml_str: str) -> bool:
@@ -365,13 +495,13 @@ class Application:
                 return n_tags
 
             ic(__verify_sub(xml, children), __num_tags(xml))
-            return __verify_sub(xml, children) == __num_tags(xml)
+            return __verify_sub(xml, children) == __num_tags(xml) and verify(xml) if verify is not None else True
         return __verify
 
     def xml_example(self, examples: str | list[str], root_tag: str, children: dict[str, str | dict[str, Any]]) -> str:
         output: str = "You have to use XML format to write your response. Examples:\n"
-        for ex in examples if type(examples) == list else [examples]:
-            output += f"{ex}\n"
+        for i, ex in enumerate(examples if type(examples) == list else [examples]):
+            output += f"Example {i}\n{ex}\n\n"
 
         output += "Valid tags:\n"
         
@@ -390,9 +520,9 @@ class Application:
 
         return __expand(children, output)
 
-    def xml_instruction_and_verify(self, examples: str | list[str], root_tag: str, children: dict[str, Any]) -> (str, Callable[[str], bool]):
+    def xml_instruction_and_verify(self, examples: str | list[str], root_tag: str, children: dict[str, Any], verify: Callable[[ET.Element], bool] = lambda x: True) -> (str, Callable[[str], bool]):
         instruction: str = self.xml_example(examples, root_tag, children)
-        verify: Callable[[str], bool] = self.verify_xml_v2(root_tag, children)
+        verify: Callable[[str], bool] = self.verify_xml_v2(root_tag, children, verify)
         return instruction, verify
 
 if __name__ == "__main__":
